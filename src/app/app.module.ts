e7a174ebb1b839d75200cc5d037a51376fe1c3f7
Manuel Gutierrez <magutbar@gmail.com>
2023-03-14 09:58:58 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Crear modulo llamado ContadorModule
+// declaraciones, y exportaciones
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador/contador.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
