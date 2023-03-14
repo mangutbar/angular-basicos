05940f311f9650348ad32333ba4d05dd30a0eba9
Manuel Gutierrez <magutbar@gmail.com>
2023-03-14 13:27:46 +0100

    Seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-// Crear modulo llamado ContadorModule
-// declaraciones, y exportaciones
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,8 @@ import { ContadorModule } from './contador/contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
